55a1f8e38e8e070753df3c930234f3a100d9e046
vallabh patidar <princepatidar107@gmail.com>
2024-02-20 21:52:47 +0530

    my first commit

--- Day7celsiusToFahrenheit.js
+++ Day7celsiusToFahrenheit.js
@@ -1,8 +1,9 @@
+
 function celToFahre(x){
-    // calculate fahrenheit //
+    // calculate fahrenheit
     return x * 9/5 + 32;
 }
-// display the result //
+// display the result
 console.log(celToFahre(20));
 console.log(celToFahre(30));
 console.log(celToFahre(40));
