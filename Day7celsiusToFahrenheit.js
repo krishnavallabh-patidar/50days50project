3d88fad9ef3fbc59949955325d0846d387f94858
krishnavallabh patidar <160020479+krishnavallabh-patidar@users.noreply.github.com>
2024-02-21 12:18:43 +0530

    Add files via upload

---
+++ Day7celsiusToFahrenheit.js
@@ -0,0 +1,9 @@
+function celToFahre(x){
+    // calculate fahrenheit //
+    return x * 9/5 + 32;
+}
+// display the result //
+console.log(celToFahre(20));
+console.log(celToFahre(30));
+console.log(celToFahre(40));
+console.log(celToFahre(73));
