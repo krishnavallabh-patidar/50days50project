e50058dc5a35b6675953190d01a1354f36e57053
vallabh patidar <princepatidar107@gmail.com>
2024-02-21 11:49:54 +0530

    file updated

--- Day7celsiusToFahrenheit.js
+++ Day7celsiusToFahrenheit.js
@@ -3,7 +3,7 @@ function celToFahre(x){
     // calculate fahrenheit
     return x * 9/5 + 32;
 }
-// display the result
+// display the result //
 console.log(celToFahre(20));
 console.log(celToFahre(30));
 console.log(celToFahre(40));
